08ce0c3a033e6d4cd373eb65b1f18b21728068c0
Yevhen Tsapko <evgentsa@gmail.com>
2023-03-30 22:34:05 +0300

    Fix year, empty responce, genres

--- src/js/fetchDataForMain.js
+++ src/js/fetchDataForMain.js
@@ -1,21 +1,6 @@
-import axios from 'axios';
-const API_KEY = 'bf6a0fae11fd4c107d4af2168346d5c6';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-
+import { downloadGenresIdList, fetchSearch } from './MovieApiSevice';
 let page = 1;
-async function downloadGenresIdList() {
-  return await axios(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`).then(
-    ({ data }) => {
-      return data.genres;
-    }
-  );
-}
-
-async function fetchSearch(page) {
-  return await axios(
-    `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${page}`
-  ).then(({ data }) => data);
-}
+const trendingUrl = '/trending/movie/day';
 
 function createGenres(genre_ids, genresIdList) {
   switch (genre_ids.length) {
@@ -26,6 +11,11 @@ function createGenres(genre_ids, genresIdList) {
         genre_ids[1],
         genresIdList
       )}`;
+    case 3:
+      return `${findGenreByID(genre_ids[0], genresIdList)}, ${findGenreByID(
+        genre_ids[1],
+        genresIdList
+      )}, ${findGenreByID(genre_ids[2], genresIdList)}`;
     default:
       return `${findGenreByID(genre_ids[0], genresIdList)}, ${findGenreByID(
         genre_ids[1],
@@ -39,14 +29,18 @@ function findGenreByID(id, genresIdList) {
   return genre.name;
 }
 
-export default async function getCardData() {
+async function getCardData() {
   const genresIdList = await downloadGenresIdList();
-  const { results } = await fetchSearch(page);
-  return results.map(({ title, poster_path, genre_ids }) => {
+  const { results } = await fetchSearch(trendingUrl, page).then(
+    ({ data }) => data
+  );
+  if (!data) return;
+  return results.map(({ title, poster_path, genre_ids, release_date }) => {
     const fullposter_path = 'https://image.tmdb.org/t/p/w500/' + poster_path;
     const genres = createGenres(genre_ids, genresIdList);
-    console.log(fullposter_path, title, genres);
-    return { fullposter_path, title, genres };
+    const release_year = release_date.slice(0, 4);
+    console.log(fullposter_path, title, genres, release_year);
+    return { fullposter_path, title, genres, release_year };
   });
 }
 getCardData();
