bb62164fb0e6b562161e418ce113e75124563526
ssulphurr <ssulphurr@gmail.com>
2023-03-30 14:29:32 +0300

    adds starter for modal

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import MovieApiSevice from './js/MovieApiSevice';
 import renderMovieCard from './js/renderMovieCard';
 import getRefs from './js/refs';
 import spiner from './js/spiner';
+import modal from './js/modal';
 
 const movieApi = new MovieApiSevice();
 const refs = getRefs();
