335a83b3e8d411d3a0873dd830487007035ca7ad
Serhii Rudenko <svr.phoenix@gmail.com>
2023-03-31 19:21:11 +0200

    Removing commented code

--- src/js/searchMovie.js
+++ src/js/searchMovie.js
@@ -43,21 +43,3 @@ async function generateMoviesList(query) {
     console.log(`Помилка:'${error}`);
   }
 }
-
-
-
-// function renderMovieCard(gallery, cards) {
-//   markup = cards
-//     .map(({ fullposter_path, title, genres, release_year }) => {
-//       return `
-//       <li class="movie-list__item">
-//         <img src=${fullposter_path} alt=${title}>
-//         <h2 class="movie-list__title">${title}</h2>
-//         <p class="movie-list__text">${genres} | ${release_year}</p>
-//       </li>
-//     `;
-//     })
-//     .join('');
-
-//   gallery.insertAdjacentHTML('beforeend', markup);
-// }
