21d515d0542f98830f2476b9927791137e8b9583
Serhii Rudenko <svr.phoenix@gmail.com>
2023-03-31 19:46:44 +0200

    Fixing bugs in index.html and refs.js after pulling

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 export default function getRefs() {
-  const searchForm = document.querySelector('.search_form');
+  const searchForm = document.querySelector('.search__form');
   const gallery = document.querySelector('.movie-list');
   const message = document.querySelector('.error_message');
   return { searchForm, gallery, message };
