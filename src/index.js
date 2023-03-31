118dbd84a34c44b5e7cef6198099cc913141e024
ssulphurr <ssulphurr@gmail.com>
2023-03-31 23:15:44 +0300

    moves modal markup to js from html

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './css/main.min.css';
 import { getCardData } from './js/fetchDataForMain';
 import { renderMovieCard } from './js/renderMovieCard';
 import { onSearchFormSubmit } from './js/searchMovie';
-import modal from './js/modal';
+import { renderModal } from './js/modal';
 import spiner from './js/spiner';
-import getRefs from './js/refs '
+import getRefs from './js/refs ';
 
 const refs = getRefs();
 
@@ -13,3 +13,5 @@ const gallery = document.querySelector('.movie-list');
 refs.searchForm.addEventListener('submit', onSearchFormSubmit);
 
 renderMovieCard(gallery, getCardData());
+
+renderModal();
