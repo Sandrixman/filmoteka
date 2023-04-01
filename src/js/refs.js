3057c9d6a83fdf6a63e168c59baac69615b4185f
Serhii Rudenko <svr.phoenix@gmail.com>
2023-04-02 00:26:27 +0200

    Some adds and bugfixes

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,5 +2,6 @@ export default function getRefs() {
   const searchForm = document.querySelector('.search__form');
   const gallery = document.querySelector('.movie-list');
   const searchInput = document.querySelector('.search__input');
-  return { searchForm, gallery, searchInput };
+  const paginationDiv = document.querySelector('.pagination');
+  return { searchForm, gallery, searchInput, paginationDiv };
 } 
