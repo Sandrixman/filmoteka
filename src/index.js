c9557b6ba76f7c95618a503641791e6b16b9611f
Oleksander Novak <sandrix@ukr.net>
2023-04-01 18:06:28 +0300

    micro update

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ import { refsModal, renderModal, removeModal } from './js/modal';
 import spiner from './js/spiner';
 import getRefs from './js/refs ';
 
-const refs = getRefs();
+const { searchForm, gallery } = getRefs();
 
-const gallery = document.querySelector('.movie-list');
-
-refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+searchForm.addEventListener('submit', onSearchFormSubmit);
 
 renderMovieCard(gallery, getCardData());
 
 // модалка фільму
-refs.gallery.addEventListener('click', renderModal);
+gallery.addEventListener('click', renderModal);
 removeModal();
