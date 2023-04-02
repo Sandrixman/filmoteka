072f4468514e9ef804f798da5fa19e9524e1b95b
ssulphurr <ssulphurr@gmail.com>
2023-04-02 17:17:36 +0300

    removes modal by clicking Escape and adds/removes listeners

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import MovieApiSevice from './js/MovieApiSevice';
 import renderMovieCard from './js/renderMovieCard';
 import getRefs from './js/refs';
 import spiner from './js/spiner';
-import modalFooter from './js/footer-modal'
+import modalFooter from './js/footer-modal';
 import { getCardData } from './js/fetchDataForMain';
 import { onSearchFormSubmit } from './js/searchMovie';
 import { renderModal } from './js/modal';
 import getRefs from './js/refs ';
-
 
 const { searchForm, gallery } = getRefs();
 
