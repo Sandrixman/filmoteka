1890817ef1a69a01606c58f8540e3e24fc6a12e3
Oleksander Novak <sandrix@ukr.net>
2023-04-03 14:15:24 +0300

    microfix

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,10 +1,9 @@
 export default function getRefs() {
-  const footerModalEl = document.querySelector(".footer-modal-js");
-  const footerAboutModalEl = document.querySelector(".about-modal-js");
-  const footerCloseBtn = document.querySelector(".about-modal__close-js");
-  const footerBackdrop = document.querySelector(".backdrop-js");
-  const bodyEl = document.querySelector("body");
   const footerModalEl = document.querySelector('.footer-modal-js');
+  const footerAboutModalEl = document.querySelector('.about-modal-js');
+  const footerCloseBtn = document.querySelector('.about-modal__close-js');
+  const footerBackdrop = document.querySelector('.backdrop-js');
+  const bodyEl = document.querySelector('body');
   const searchForm = document.querySelector('.search__form');
   const gallery = document.querySelector('.movie-list');
   const searchInput = document.querySelector('.search__input');
@@ -30,7 +29,7 @@ export default function getRefs() {
     footerAboutModalEl,
     footerCloseBtn,
     footerBackdrop,
-    bodyEl
+    bodyEl,
     backdropTrailer,
     playerDiv,
     footerModalEl,
